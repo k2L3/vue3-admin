62f6381d5d9280445c1f567ece328cdc8ba5d3ad
like <1015641154@qq.com>
2023-06-05 16:32:47 +0800

    feat：添加设计概览

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: LikSeven
+ * @Date: 2023-05-30 09:35:40
+ * @LastEditTime: 2023-06-05 16:28:58
+ * @LastEditors: LikSeven
+ * @Description: 
+ * @FilePath: \vue3-admin\src\router\index.ts
+ */
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
 
 const BASE_URL : string = import.meta.env.BASE_URL;
@@ -10,6 +18,14 @@ const routes: Array<RouteRecordRaw> = [
             title: '首页'
         },
         component: () => import("@/views/page-index/index.vue")
+    }, 
+    {
+        path: '/login',
+        name: 'login',
+        meta: {
+            title: '登录'
+        },
+        component: () => import("@/views/login/index.vue")
     }
 ];
 
--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    server: {
+        open: true
+    },
     plugins: [
         vue(),
          //importStyle可以配置element-plus的样式引入方式，它默认是css，利用scss变量修改主题时，需要将这个属性设置为scss
